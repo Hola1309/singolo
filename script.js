2466d35c47ec0d2ba709460a27b9824230a03c36
Hola <hola131098@gmail.com>
2020-03-19 22:01:51 +0400

    feat: singolo DOM task update

--- script.js
+++ script.js
@@ -3,9 +3,20 @@ const strlkR = document.getElementById('strlkR');
 const phon1 = document.getElementById('phon1');
 const phon2 = document.getElementById('phon2');
 const send = document.getElementById('send');
-const close = document.getElementById('close')
+const close = document.getElementById('close');
+const filter = document.getElementById('filter');
+const artG = document.getElementById('artG');
+const name = document.getElementById('name');
+const email = document.getElementById('email');
 var cordX, cordY, ph1 = 0, ph2 = 0;
 
+
+
+artG.addEventListener('click', (event) => {
+	artG.querySelectorAll('img').forEach(el => el.classList.remove('artActive'));
+	if (event.target.className != "art_gallery") {
+	event.target.classList.add('artActive');}
+})
 
 	var canvas = document.getElementById("canvas"),
         ctx = canvas.getContext('2d'),
@@ -181,7 +192,22 @@ strlkR.addEventListener('click', (event) => {
 
 
 
-
+filter.addEventListener('click', (event) => {
+	var arrOfSrc = [];
+	var arrOfPic = ["pic1", "pic2", "pic3", "pic4", "pic5", "pic6", "pic7", "pic8", "pic9", "pic10", "pic11", "pic12"];
+	for (var i = 0; i < arrOfPic.length; i++) {
+		arrOfSrc.push(document.getElementById(arrOfPic[i]).src)
+	}
+	for (var i = arrOfSrc.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = arrOfSrc[i];
+        arrOfSrc[i] = arrOfSrc[j];
+        arrOfSrc[j] = temp;
+    }
+    for (var i = 0; i < arrOfPic.length; i++) {
+    	document.getElementById(arrOfPic[i]).src = arrOfSrc[i]
+    }
+})
 
 
 
@@ -193,16 +219,21 @@ strlkR.addEventListener('click', (event) => {
 
 close.addEventListener('click', (event)=> {
 	document.getElementById("get").className = 'gethid';
+	name.value = '';
+	email.value = '';
+	document.getElementById('dis').value = '';
+	document.getElementById('sub').value = '';
 });
 
 send.addEventListener('click', (event) =>  {
+	if (name.value != '' && email.value != '') {
 	var a = document.getElementById('sub').value.toString();
 	if (a == '')
-	{document.getElementById('subreturn').innerText = 'Без темы'} else {document.getElementById('subreturn').innerText = a}
+	{document.getElementById('subreturn').innerText = 'Без темы'} else {document.getElementById('subreturn').innerText = 'Тема: ' + a}
 	a = document.getElementById('dis').value.toString();
 	if (a == '')
-	{document.getElementById('disreturn').innerText = 'Без описания'} else {document.getElementById('disreturn').innerText = a}
+	{document.getElementById('disreturn').innerText = 'Без описания'} else {document.getElementById('disreturn').innerText = 'Описание: ' + a}
 	
-	document.getElementById("get").className = "get";
+	document.getElementById("get").className = "get";} else {alert('Ошибка, заполните поле name и email')}
 });
 
